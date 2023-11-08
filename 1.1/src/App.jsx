5b55a80eef93dfbbf0a55aba8447f1d6a8c391ed
miia6 <128396444+miia6@users.noreply.github.com>
2023-11-08 15:56:22 +0200

    Add files via upload

---
+++ 1.1/src/App.jsx
@@ -0,0 +1,43 @@
+
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
+};
+
+const Content = ({ content }) => {
+  return (
+    <div>
+      { content.forEach( element => {
+          <p>{element.part} {element.exercises}</p>
+        })
+      }
+    </div>
+  );
+};
+
+
+const Total = ({ content }) => {
+  const totalExercises = content[0].exercises + content[1].exercises + content[2].exercises;
+  return <p>Number of exercises {totalExercises}</p>;
+};
+
+const App = () => {
+
+  const course = 'Half Stack application development';
+  const content = [
+    { part: 'Fundamentals of React', exercises: 10 },
+    { part: 'Using props to pass data', exercises: 7},
+    { part: 'State of a component', exercises: 14}
+  ];
+  
+  return (
+    <div>
+      <Header course={course} />
+      <Content content={content} />
+      <Total content={content} />
+    </div>
+  )
+
+};
+
+export default App
+
