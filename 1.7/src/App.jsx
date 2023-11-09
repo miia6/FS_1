992eed7b3c6bce40e35bca5ae53cb45078c2313a
miia6 <128396444+miia6@users.noreply.github.com>
2023-11-09 10:35:01 +0200

    Add files via upload

---
+++ 1.7/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [allClicks, setAll] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  const handleGoodClick = () => {
+    setGood(good + 1);
+    setTotal(total + 1);
+    setAll(allClicks.concat('G'));
+  };
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+    setTotal(total + 0);
+    setAll(allClicks.concat('N'));
+  };
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+    setTotal(total - 1);
+    setAll(allClicks.concat('B'));
+  };
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <button onClick={handleBadClick}>bad</button>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {allClicks.length}</p>
+      <p>average {total/allClicks.length}</p>
+      <p>positive {good/allClicks.length}</p>
+    </div>
+  )
+};
+
+export default App
